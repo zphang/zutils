5ffdf90c9c27f46e37f6946fffb1e307c8456297
Jason Phang <email@jasonphang.com>
2019-06-25 18:13:08 +0800

    working zsync

--- zsync/zsync.py
+++ zsync/zsync.py
@@ -4,11 +4,18 @@ import subprocess
 
 from pyutils.io import read_json
 
-ZSYNC_CONF_NAME = ".zsync.conf"
+ZSYNC_CONF_NAME = ".zsync.json"
 
 
-def load_config():
-    return read_json(ZSYNC_CONF_NAME)
+def load_config(location):
+    if not os.path.exists(ZSYNC_CONF_NAME):
+        raise FileNotFoundError(ZSYNC_CONF_NAME)
+    all_config = read_json(ZSYNC_CONF_NAME)
+    if not all_config:
+        raise RuntimeError("Config file is empty: {}".format(os.path.abspath(ZSYNC_CONF_NAME)))
+    if location not in all_config:
+        raise KeyError("'{}' config not found".format(location))
+    return all_config[location]
 
 
 def construct_rsync_tokens(ssh_key_path, exclude_list, delete, src, dst):
@@ -30,21 +37,25 @@ def construct_rsync_tokens(ssh_key_path, exclude_list, delete, src, dst):
         tokens.append("--exclude='{}'".format(exclude))
     if delete:
         tokens.append("--delete")
-    tokens.append(src, dst)
+    tokens.append(src)
+    tokens.append(dst)
     return tokens
 
 
 def perform_command(tokens, show):
+    formatted_command = " \\\n    ".join(tokens)
     if show:
-        print(" \\\n    ".join(tokens))
+        print(formatted_command)
     else:
-        raise RuntimeError()
-        result = subprocess.run(tokens, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+        result = subprocess.run(
+            formatted_command,
+            stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=True,
+        )
         print(result.stdout.decode('utf-8'))
 
 
 def zsync_to(location, show):
-    location_config = read_json(ZSYNC_CONF_NAME)[location]
+    location_config = load_config(location)
     src = location_config["local_path"]
     dst = "{}@{}:{}".format(
         location_config["remote_user"],
@@ -62,7 +73,7 @@ def zsync_to(location, show):
 
 
 def zsync_from(location, show):
-    location_config = read_json(ZSYNC_CONF_NAME)[location]
+    location_config = load_config(location)
     src = "{}@{}:{}".format(
         location_config["remote_user"],
         location_config["remote_host"],
@@ -83,8 +94,18 @@ def main():
     parser = argparse.ArgumentParser(description='PyTorch ImageNet Training')
     parser.add_argument('mode', help='to, from')
     parser.add_argument('location')
-    parser.add_argument('--show')
+    parser.add_argument('--show', action="store_true")
     args = parser.parse_args()
+    if args.mode == "to":
+        zsync_to(
+            location=args.location,
+            show=args.show,
+        )
+    elif args.mode == "from":
+        zsync_from(
+            location=args.location,
+            show=args.show,
+        )
 
 
 if __name__ == "__main__":
